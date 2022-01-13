7d626d36336ffaf723e16b477e0333e3dd563498
Rocco Wu <rw2314@nyu.edu>
2022-01-13 02:52:10 -0800

    Input System Working
    - Implemented the Input System Interface.
    - Added a temporary reflector
    - Working on getting the hinges to work

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, PlayerInput.IPlayerActions
 {
+    private PlayerInput playerInput;
+    private PlayerInput playerInput_
+    {
+        get  
+        {
+            playerInput ??= new PlayerInput(); //creates new playerInput when the player input is null
+            return playerInput;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,37 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+     private void OnEnable()
+    {
+        playerInput_.Enable();
+        playerInput_.Player.SetCallbacks(this);
+    }
+    private void OnDisable()
+    {
+        playerInput_.Disable();       
+    }
+
+    public void OnFlipperRight(InputAction.CallbackContext context)
+    {
+        if (context.phase != InputActionPhase.Canceled)
+        {
+            print("flipper right");  
+        }            
+    }
+    public void OnFlipperLeft(InputAction.CallbackContext context)
+    {
+        if (context.phase != InputActionPhase.Canceled)
+        {
+            print("flipper left");  
+        }   
+    }
+
+    public void OnBallLauncher(InputAction.CallbackContext context)
+    {
+          if (context.phase != InputActionPhase.Canceled)
+        {
+            print("launch ball");  
+        }    
     }
 }
